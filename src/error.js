16e8e9249d8aff3c41fb7a507d5d70b90854ca12
Rafael Becker <realfabecker@gmail.com>
2021-07-07 20:35:00 -0300

    chore: removido optional chaining

--- src/error.js
+++ src/error.js
@@ -1,10 +1,5 @@
 const HttpStatus = require('http-status-codes')
 
-/**
- * @param status
- * @param message
- * @returns {{error: string, message, status: *}|{error: string, status: *}}
- */
 const toUnifiedError = ({ status, message = '' }) => {
     const error = HttpStatus.getReasonPhrase(status)
     const payload = { status, error }
@@ -14,10 +9,6 @@ const toUnifiedError = ({ status, message = '' }) => {
     return payload
 }
 
-/**
- * @param err
- * @returns {{isBoom}|*|Boom<unknown>}
- */
 const getTransformedError = (err) => {
     if (err.isBoom) {
         return toUnifiedError({
@@ -26,8 +17,9 @@ const getTransformedError = (err) => {
         })
     } else if (err.isAxiosError) {
         return toUnifiedError({
-            status: err?.response?.status || HttpStatus.INTERNAL_SERVER_ERROR,
-            message: err?.response?.data?.message || '',
+            status: err && err.response && err.response.status || HttpStatus.INTERNAL_SERVER_ERROR,
+            message: err && err.response && err.response.data && err.response.data.message ||
+                HttpStatus.getReasonPhrase(HttpStatus.INTERNAL_SERVER_ERROR),
         })
     } if (Array.isArray(err.errors)) {
         const message = err.errors
@@ -54,10 +46,10 @@ const getTransformedError = (err) => {
     }
 }
 
-module.exports = (logger) =>  (err, req, res, next) => {
+module.exports = (callback) =>  (err, req, res, next) => {
     const { status, error, message = '' } = getTransformedError(err)
 
-    logger && logger(req, res, {status, error, message})
+    callback && callback(req, res, {status, error, message})
 
     if (status === HttpStatus.UNAUTHORIZED) {
         return res.status(status).send()
